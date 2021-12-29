0c0211cb5e1b2492feb19b88054ee3d168c970f2
youssefbadaoui89 <badaouiyoussef8888@gmail.com>
2021-12-29 14:47:55 +0000

    root26_decrypter

--- rot26.c
+++ rot26.c
@@ -6,14 +6,13 @@ int main()
 	char *str;
 
 	i = 0;
-	printf("hey friend.....\nwelcome to ARB26....\nnow quicly give me your dumb string...\ni feel bored solving this shiit...\n\n\nyour string here please:\n");
+	printf("hey friend.....:)\nwelcome to ARB26....\n.\n.\n.\nplease your encoded root26 string here:\n");
      	gets(str);
 	while(str[i])
 	{
 		str[i] -= 26;
 		if(str[i] < 33)
-			str[i] += 94; 
-	//	normal(str);
+			str[i] += 94;
 		i++;
 	}
 	printf("%s", str);
